0c6e57eacbedb1e4f8339b67a6570f1b15449a58
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-08-10 14:01:02 +0100

    added method to save user's bookmark in BookmarkDao class

--- src/mvc/dao/BookmarkDao.java
+++ src/mvc/dao/BookmarkDao.java
@@ -2,9 +2,14 @@ package mvc.dao;
 
 import mvc.database.DataStore;
 import mvc.entities.Bookmark;
+import mvc.entities.UserBookmark;
 
 public class BookmarkDao {
     public Bookmark[][] getBookmarks() {
         return DataStore.getBookmarks();
+    }
+
+    public void saveUserBookmark(UserBookmark userBookmark) {
+        DataStore.add(userBookmark);
     }
 }
