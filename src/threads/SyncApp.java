ff48564748f770cafbae8af04402dfc1a79ed8b1
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-21 10:51:28 +0100

    allowed printDocuments function sleep for 5 seconds

--- src/threads/SyncApp.java
+++ src/threads/SyncApp.java
@@ -29,6 +29,11 @@ public class SyncApp {
 class Printer {
     void printDocuments(int numOfCopies, String docName) {
         for (int i = 1; i <= numOfCopies; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Printing " + docName + i);
         }
     }
