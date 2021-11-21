5002778509875f814da0ce2d2fc0c81057c07fbf
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2021-11-21 12:17:58 +0100

    add synchronized keyword to Printer and Downloader tasks functions

--- src/threads/SyncApp.java
+++ src/threads/SyncApp.java
@@ -8,7 +8,7 @@ public class SyncApp {
         Printer printer = new Printer();
         Thread examResults =
                 new Thread(() -> {
-                    printer.printDocuments(100, "Exam Results ");
+                    printer.printExamDocuments(10, "Exam Results ");
                     printer.printPdfFiles(20);
                 });
         examResults.start();
@@ -16,10 +16,11 @@ public class SyncApp {
         Downloader movieDownloader = new Downloader();
         Thread downloadMovies =
                 new Thread(() -> {
-                    movieDownloader.movieDownloader(100, "Prison Break Series ");
+                    movieDownloader.movieDownloader(100, "DC movies collections ");
                     movieDownloader.seriesDownloader(30, 2, "Game Of Thrones ");
                 });
         downloadMovies.start();
+
         System.out.println("Application Finished");
 
     }
@@ -27,33 +28,28 @@ public class SyncApp {
 }
 
 class Printer {
-    void printDocuments(int numOfCopies, String docName) {
+    synchronized void printExamDocuments(int numOfCopies, String docName) {
         for (int i = 1; i <= numOfCopies; i++) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             System.out.println("Printing " + docName + i);
         }
     }
 
-    void printPdfFiles(int numOfCopies) {
-        for (int i = 1; i < numOfCopies; i++)
-            System.out.println("Printing " + i + " pdfs ");
+    synchronized void printPdfFiles(int numOfCopies) {
+        for (int i = 1; i <= numOfCopies; i++)
+            System.out.println("Printing " + i + " pdfs files");
 
     }
 
 }
 
 class Downloader {
-    void movieDownloader(int numOfDownloads, String movieTitle) {
-        for (int i = 1; i < numOfDownloads; i++) {
+    synchronized void movieDownloader(int numOfDownloads, String movieTitle) {
+        for (int i = 1; i <= numOfDownloads; i++) {
             System.out.println("Downloading " + movieTitle + i);
         }
     }
 
-    void seriesDownloader(int numberOfEpisodes, int numOfSeason, String seriesTitle) {
+    synchronized void seriesDownloader(int numberOfEpisodes, int numOfSeason, String seriesTitle) {
         if (numOfSeason > 0) {
             for (int i = 1; i <= numberOfEpisodes; i++) {
                 System.out.println("Downloading " + i + " of " + numberOfEpisodes + " episodes from " + seriesTitle + numOfSeason);
